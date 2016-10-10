ba442bf86bc7ed6ae5711645faa0d468125b9770
Tiago Davi <tiago.asp.net@gmail.com>
2016-10-10 09:14:15 -0300

    v1 add react router

--- src/app.js
+++ src/app.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import * as reducers from './reducers';
 import App from './components/app';
-import Sidebar from './components/sidebar';
 
-const store = createStore(combineReducers(reducers));
+reducers.routing = routerReducer;
+
+const store   = createStore(combineReducers(reducers));
+const history = syncHistoryWithStore(browserHistory, store);
+const routes  = (<Route path='/' component={ App } />);
 
 function run() {
   let state = store.getState();
   ReactDOM.render(
     <Provider store={ store }>
-      <App>
-       <Sidebar />
-      </App>
+      <Router history={ history }>
+        { routes }
+      </Router>
     </Provider>,
     document.getElementById('root')
   );
